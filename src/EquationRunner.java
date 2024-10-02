d41ffc57f87e0d0c10158063f718f5282a0b8380
BT_2S14_31 <adnerc@nycstudents.net>
2024-10-02 10:54:33 -0400

    Program still not finished, must complete fraction work. Current fraction function used from https://www.baeldung.com/java-decimal-fraction-conversion

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -4,26 +4,38 @@ import java.text.DecimalFormat;
 public class EquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
         System.out.print("What is your first pair? ");
-        String xString = s.nextLine();
+        String firstString = s.nextLine();
 
         System.out.print("What is your second pair? ");
-        String yString = s.nextLine();
+        String secondString = s.nextLine();
 
-        System.out.print("What is your second x value? ");
-        String x2String = s.nextLine();
+        int index1 = firstString.indexOf(",");
+        int index2 = secondString.indexOf(",");
 
-        System.out.print("What is your second y value? ");
-        String y2String = s.nextLine();
+        String x1S = firstString.substring(1, index1);
+        String y1S = firstString.substring(index1+1, firstString.length()-1);
 
-//        int indexOf;(xString, ",");
+        String x2S = secondString.substring(1, index1);
+        String y2S = secondString.substring(index2+1, secondString.length()-1);
 
-        int x = Integer.parseInt(xString);
-        int x2 = Integer.parseInt(x2String);
-        int y = Integer.parseInt(yString);
-        int y2 = Integer.parseInt(y2String);
+        int x1 = Integer.parseInt(x1S);
+        int y1 = Integer.parseInt(y1S);
+        int x2 = Integer.parseInt(x2S);
+        int y2 = Integer.parseInt(y2S);
 
-        LinearEquation firstEquation = new LinearEquation(x,y,x2,y2);
+        LinearEquation firstEquation = new LinearEquation(x1,y1,x2,y2);
+
+        String printString = firstEquation.toString();
+        System.out.println(printString);
+
+        System.out.print("What is your new x value? ");
+        String newXString = s.nextLine();
+        int newX = Integer.parseInt(newXString);
+        LinearEquation secondEquation = new LinearEquation(x1,y1,x2,y2, newX);
+        double newY = secondEquation.newX();
+        System.out.println("Calculated point: ("+newX+","+newY+")");
 
 
 
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 public class LinearEquation {
     private int x;
     private int y;
@@ -19,31 +20,51 @@ public class LinearEquation {
         this.x2 = x2;
         this.y2 = y2;
         this.x3 = x3;
+    }
+
+    public String fractionConversion( double number ){
+        String slope = null;
+
+        String numberString = number+"";
+        int decPlace = numberString.length() - numberString.indexOf(".")-1;
+        long denominator = (long) Math.pow(10, decPlace);
+        long numerator =(long) (number*denominator);
+        if (numerator % denominator == 0) {
+            slope = number/denominator+"";
+        } else {
+            slope = numerator+"/"+denominator;
+        }
+        return slope;
 
     }
-    public double SlopeCalc() {
+
+
+    public double slopeCalc() {
         double slope = ((double) y2 - y) / ((double) x2 - x);
 
         return slope;
     }
 
-    public double InterceptCalc() {
-        double b = y - SlopeCalc();
+    public double interceptCalc() {
+        double b = y - slopeCalc()*x;
         return b;
     }
 
-    public double Distance() {
+    public double distance() {
         double distance = Math.sqrt(Math.pow(x2 - x, 2) + Math.pow((y2 - y), 2)); // dividing the total amount by the amount of p
         return distance;
     }
 
-    public double NewX() {
-        return (SlopeCalc() * x3) + InterceptCalc();
+    public double newX() {
+        return (slopeCalc() * x3) + interceptCalc();
     }
 
     public String toString() {
-        String info = "First pair: " +"("+x+", "+y+")" + "\n"  ;
-        return null;
+        DecimalFormat df = new DecimalFormat("#.##"); // used to round future numbers/variables
+
+
+        String info = "First pair: " +"("+x+", "+y+")" + "\n"+"Second pair: " +"("+x2+", "+y2+")"+"\n"+"Slope of line : "+fractionConversion(slopeCalc())+"\n"+"Y-Intercept: "+interceptCalc()+"\n"+"Slope intercept form: y = "+fractionConversion(slopeCalc())+"x + "+interceptCalc()+"\n"+"Distance between points: "+ df.format(distance());
+        return info;
     }
 
 
