b39935e165a925bfaa9fba3d5b3bb5220a91f8f8
BT_2S14_31 <adnerc@nycstudents.net>
2024-10-07 10:58:00 -0400

    Program IS NOT mostly finished, polish and comments soon. Current fraction function used from https://www.baeldung.com/java-decimal-fraction-conversion.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,48 +1,34 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-
 public class EquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-
         System.out.print("What is your first pair? ");
         String firstString = s.nextLine();
-
 
         System.out.print("What is your second pair? ");
         String secondString = s.nextLine();
 
-
         int index1 = firstString.indexOf(", ");
         int index2 = secondString.indexOf(", ");
-
 
         String x1S = firstString.substring(1, index1);
         String y1S = firstString.substring(index1+2, firstString.length()-1);
 
-
         String x2S = secondString.substring(1, index2);
         String y2S = secondString.substring(index2+2, secondString.length()-1);
-
 
         int x1 = Integer.parseInt(x1S);
         int y1 = Integer.parseInt(y1S);
         int x2 = Integer.parseInt(x2S);
         int y2 = Integer.parseInt(y2S);
 
-
         LinearEquation firstEquation = new LinearEquation(x1,y1,x2,y2);
-
-
-        String testString = firstEquation.test();
-        System.out.println(testString);
-
 
         String printString = firstEquation.toString();
         System.out.println(printString);
-
 
         System.out.print("What is your new x value? ");
         String newXString = s.nextLine();
@@ -50,7 +36,6 @@ public class EquationRunner {
         LinearEquation secondEquation = new LinearEquation(x1,y1,x2,y2, newX);
         double newY = secondEquation.newX();
         System.out.println("Calculated point: ("+newX+","+newY+")");
-
 
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -6,7 +6,7 @@ public class LinearEquation {
     private int y2;
     private int x3;
 
-
+    // Constructor for when there is no x3
     public LinearEquation(int x, int y, int x2, int y2) {
         this.x = x;
         this.y = y;
@@ -16,6 +16,8 @@ public class LinearEquation {
 
 
     }
+    // Constructor for when there is x3
+
     public LinearEquation(int x, int y, int x2, int y2, int x3) {
         this.x = x;
         this.y = y;
@@ -24,100 +26,94 @@ public class LinearEquation {
         this.x3 = x3;
     }
 
-
+    // converts numbers to fractions
     public String fractionConversion( double number ){
-        String slope;
+        String slope; //creating the variables
+        String numberString = number+""; //converting the number to be converted in to a string
 
+        int decPlace = numberString.length() - numberString.indexOf(".")-2; //find the number of digits to the right of the decimal EX:
+        long denominator = (long) Math.pow(10, decPlace); // denominator is 10 to the power of that many digits
+        long numerator =(long) (number*denominator);//The numerator is the denominator multiplied by the number to be divided
 
-        String numberString = number+"";
-        int decPlace = numberString.length() - numberString.indexOf(".")-1;
-        long denominator = (long) Math.pow(10, decPlace);
-        long numerator =(long) (number*denominator);
-        if (numerator % denominator == 0) {
+        if (numerator % denominator == 0) { //checks if the number is an integer
             slope = number/denominator+"";
-        } else {
+        } else { // if not, then just set slope equal to
             slope = numerator+"/"+denominator;
         }
-        if (slopeCalc()<0) {
-            slope =  numerator+"/"+(denominator);
-
-
+        if (number/denominator == 0) {
+            slope = "";
         }
-
 
         return slope;
 
-
     }
 
-
-
-
-    public double slopeCalc() {
+    public double slopeCalc() { //calculates slope
         double slope = ((double) y2 - y) / ((double) x2 - x);
 
-
         return slope;
     }
 
 
-    public double interceptCalc() {
-        double b = y - slopeCalc()*x;
-        if (b<0){
-            return (-1*b);
-        }
+    public double interceptCalc() { //calculates the y - intercept
+        double b = y - slopeCalc()*x; // b is the intercept
+
         return b;
     }
 
 
-    public double distance() {
+    public double distance() { //calculates the distance between points
         double distance = Math.sqrt(Math.pow(x2 - x, 2) + Math.pow((y2 - y), 2)); // dividing the total amount by the amount of p
         return distance;
     }
 
 
-    public double newX()
+    public double newX() // returns the y value associated with the x
     {
         return (slopeCalc() * x3) + interceptCalc();
     }
 
-    public String toString() {
-        DecimalFormat df = new DecimalFormat("#.##"); // used to round future numbers/variables
-        if (slopeCalc()==1) {
-            String info = "First pair: " +"("+x+", "+y+")" + "\n"+"Second pair: " +"("+x2+", "+y2+")"+"\n"+"Slope of line : "+fractionConversion(slopeCalc())+"\n"+"Y-Intercept: "+interceptCalc()+"\n"+"Slope intercept form: y = "+" x + "+interceptCalc()+"\n"+"Distance between points: "+ df.format(distance());
-            return info;
-
-
-        }
-        if (slopeCalc()==-1) {
-            String info = "First pair: " + "(" + x + ", " + y + ")" + "\n" + "Second pair: " + "(" + x2 + ", " + y2 + ")" + "\n" + "Slope of line : " + fractionConversion(slopeCalc()) + "\n" + "Y-Intercept: " + interceptCalc() + "\n" + "Slope intercept form: y = " + " -x + " + interceptCalc() + "\n" + "Distance between points: " + df.format(distance());
-            return info;
-
-
-        }
-        if (interceptCalc()==0) {
-            String info = "First pair: " + "(" + x + ", " + y + ")" + "\n" + "Second pair: " + "(" + x2 + ", " + y2 + ")" + "\n" + "Slope of line : " + fractionConversion(slopeCalc()) + "\n" + "Y-Intercept: " + interceptCalc() + "\n" + "Slope intercept form: y = " + slopeCalc() + "x"  + "\n" + "Distance between points: " + df.format(distance());
-            return info;
-
-
-        }
-        if (slopeCalc()==0 && interceptCalc()==0){
-            String info = "First pair: " + "(" + x + ", " + y + ")" + "\n" + "Second pair: " + "(" + x2 + ", " + y2 + ")" + "\n" + "Slope of line : " + fractionConversion(slopeCalc()) + "\n" + "Y-Intercept: " + interceptCalc() + "\n" + "Slope intercept form: y = 0" + "\n" + "Distance between points: " + df.format(distance());
-            return info;
-
-
-        }
-        if (interceptCalc()<0) {
-            String info = "First pair: " + "(" + x + ", " + y + ")" + "\n" + "Second pair: " + "(" + x2 + ", " + y2 + ")" + "\n" + "Slope of line : " + fractionConversion(slopeCalc()) + "\n" + "Y-Intercept: " + interceptCalc() + "\n" + "Slope intercept form: y = " + fractionConversion(slopeCalc()) + "x - " + interceptCalc() + "\n" + "Distance between points: " + df.format(distance());
-            return info;
-        }
-        String info = "First pair: " +"("+x+", "+y+")" + "\n"+"Second pair: " +"("+x2+", "+y2+")"+"\n"+"Slope of line : "+fractionConversion(slopeCalc())+"\n"+"Y-Intercept: "+interceptCalc()+"\n"+"Slope intercept form: y = "+fractionConversion(slopeCalc())+"x + "+interceptCalc()+"\n"+"Distance between points: "+ df.format(distance());
-        return info;
-    }
-    public String test() {
-
-
+    public String test() {// used for testing conversion/slope
         String test = "Slope before fraction conversion: " + slopeCalc()+"\n"+"Formatted slope: " + fractionConversion(slopeCalc());
         return test;
     }
+
+    public String toString() {// returns a string with all the wanted information
+
+        DecimalFormat df = new DecimalFormat("#.##"); // used to round future numbers/variables
+        String firstPair = "First pair: " +"("+x+", "+y+")";
+        String secondPair = "Second pair: " +"("+x2+", "+y2+")";
+        String slope = "Slope of line : "+fractionConversion(slopeCalc());
+        String intercept = "Y-Intercept: "+interceptCalc();
+        String equation ="Slope intercept form: y = "+fractionConversion(slopeCalc())+"x + "+interceptCalc();
+        String distance = "Distance between points: "+ df.format(distance());
+
+        String slopeData = fractionConversion(slopeCalc());
+        String interceptData = interceptCalc()+"";
+        String distanceData = "Distance between points: "+ df.format(distance());
+
+        if (slopeCalc()==1) {
+            slopeData ="";
+        }
+        if (slopeCalc()==-1) {
+            slopeData ="";
+        }
+        if (interceptCalc()==0) {
+            interceptData = "";
+            equation ="Slope intercept form: y = "+fractionConversion(slopeCalc())+"x";
+        }
+        if (slopeCalc()==0){
+            slopeData = "";
+        }
+        if (slopeCalc()==0 && interceptCalc()==0){
+            equation ="Slope intercept form: y = "+fractionConversion(slopeCalc())+"x - "+interceptCalc()*-1;
+        }
+        if (interceptCalc()<0) { // if the intercept is negative, make it positive and simply subtract
+            equation ="Slope intercept form: y = "+fractionConversion(slopeCalc())+"x";
+        }
+        String finalEquation = equation;
+        String info = "First pair: " +"("+x+", "+y+")" + "\n"+"Second pair: " +"("+x2+", "+y2+")"+"\n"+"Slope of line : "+fractionConversion(slopeCalc())+"\n"+"Y-Intercept: "+interceptCalc()+"\n"+"Slope intercept form: y = "+fractionConversion(slopeCalc())+"x + "+interceptCalc()+"\n"+"Distance between points: "+ df.format(distance());
+        return info;
+    }
+
 }
