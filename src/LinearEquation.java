25d98c3d122c57e4ceaad3d981d5a7dc930a4f7e
AdnerC <148999774+AdnerC@users.noreply.github.com>
2024-10-10 21:57:20 -0400

    Update LinearEquation.java
    Program done, polish, comments, and proper formatting have been completed. Most likely final version

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -7,30 +7,32 @@ public class LinearEquation {
     private int y2;
     private int x3;
 
-    // Constructor for when there is no x3
-    public LinearEquation(int x, int y, int x2, int y2) {
+
+    public LinearEquation(int x, int y, int x2, int y2) {    // Constructor for when there is no x3
+
+        this.x = x;
+        this.y = y;
+        this.x2 = x2;
+        this.y2 = y2;
+
+    }
+
+
+    public LinearEquation(int x, int y, int x2, int y2, int x3) {    // Constructor for when there is x3
+
         this.x = x;
         this.y = y;
         this.x2 = x2;
         this.y2 = y2;
         this.x3 = x3;
-
-    }
-    // Constructor for when there is x3
-
-    public LinearEquation(int x, int y, int x2, int y2, int x3) {
-        this.x = x;
-        this.y = y;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.x3 = x3;
     }
 
-    // converts numbers to fractions
-    public String fractionConversion( double number ){
+
+    public String fractionConversion( double number ){    // converts numbers to fractions
+
         String slope; //creating the variables
         String numberString = number+""; //converting the number to be converted in to a string
-
+        // long is used for 2 of its properties, being able to represent a large number (useful for line 38 since 10^(decPlace) may get large, as well as being able to represent large integers without having to worry about floating point
         int decPlace = numberString.length() - numberString.indexOf(".")-2; //find the number of digits to the right of the decimal EX:
         long denominator = (long) Math.pow(10, decPlace); // denominator is 10 to the power of that many digits
         long numerator =(long) (number*denominator);//The numerator is the denominator multiplied by the number to be divided
@@ -40,13 +42,14 @@ public class LinearEquation {
         } else { // if not, then just set slope equal to
             slope = numerator+"/"+denominator;
         }
-        if (number/denominator == 0) {
+        if (number/denominator == 0) {// if there is no slope, just return ""
             slope = "";
         }
 
         return slope;
 
     }
+
 
     public double slopeCalc() { //calculates slope
         double slope = ((double) y2 - y) / ((double) x2 - x);
@@ -73,10 +76,6 @@ public class LinearEquation {
         return (slopeCalc() * x3) + interceptCalc();
     }
 
-    public String test() {// used for testing conversion/slope
-        String test = "Slope before fraction conversion: " + slopeCalc()+"\n"+"Formatted slope: " + fractionConversion(slopeCalc());
-        return test;
-    }
 
     public String toString() {// returns a string with all the wanted information
 
@@ -98,9 +97,12 @@ public class LinearEquation {
             interceptData = df.format(interceptValue); // Format if not 0
         }
 
-        String distanceData = "Distance between points: " + df.format(distance());
+        String distanceData = "Distance between points: " + df.format(distance());// creates the distance string
 
-        if (slopeValue == 1) {
+        if (y == y2) {// returns a different string if there is no function and only a constant
+            return firstPair + "\n" + secondPair + "\n" + "Equation of the line: y = " + y + "\n" + distanceData;
+        }
+        if (slopeValue == 1) {//all of these are used for formatting depending on the status of slopeValue
             slopeData = "";
         }
         if (slopeValue == -1) {
@@ -110,7 +112,7 @@ public class LinearEquation {
             slopeData = "";
         }
 
-        String equation;
+        String equation;// making the different formats for the equation depending on the intercept value
         if (interceptValue > 0) {
             equation = "Slope intercept form: y = " + slopeData + "x + " + interceptData;
         } else if (interceptValue < 0) {
@@ -119,10 +121,11 @@ public class LinearEquation {
             equation = "Slope intercept form: y = " + slopeData + "x";
         }
 
-        if (Integer.parseInt(slopeData) == 0 ){
-            slopeData ="1";
+        if (slopeValue == 1) {
+            slopeData = "1"; // this makes it so that in the equation the slope does not appear, but the slope still appears in the slope of line print statement
         }
 
+        //creating the final string so it can be used elsewhere
         String info = firstPair + "\n" + secondPair + "\n" + "Slope of line: " + slopeData + "\n" + "Y-Intercept: " + df.format(interceptValue) + "\n" + equation + "\n" + distanceData;
         return info;
 
